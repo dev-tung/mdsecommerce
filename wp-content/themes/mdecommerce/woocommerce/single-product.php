027a89cd01e53d36891357c3b260a9db116300a3
dev-tung <devtung27101992@gmail.com>
2026-01-19 16:49:10 +0700

    Update code

--- wp-content/themes/mdecommerce/woocommerce/single-product.php
+++ wp-content/themes/mdecommerce/woocommerce/single-product.php
@@ -85,47 +85,52 @@ if ( ! $product ) return;
 <div class="container-fluid p-lg-4 p-2">
   <div class="row gx-2">
 
-    <!-- =====================
-         IMAGE SECTION (8/12)
-    ====================== -->
-    <div class="col-lg-8">
-      <div class="row g-2">
+  <!-- =====================
+      IMAGE SECTION (8/12)
+  ====================== -->
+  <div class="col-lg-8">
+    <div class="row g-2">
 
-        <?php
-        // Lấy ảnh gallery + ảnh đại diện
-        $gallery_ids = $product->get_gallery_image_ids();
-        $main_image  = $product->get_image_id();
+      <?php
+      // Lấy ảnh gallery + ảnh đại diện
+      $gallery_ids = $product->get_gallery_image_ids();
+      $main_image  = $product->get_image_id();
 
-        if ( $main_image ) {
-          array_unshift( $gallery_ids, $main_image );
-        }
+      if ( $main_image ) {
+        array_unshift( $gallery_ids, $main_image );
+      }
 
-        // Nếu không có ảnh
-        if ( empty( $gallery_ids ) ) :
-        ?>
-          <div class="col-12">
-            <img src="<?php echo wc_placeholder_img_src(); ?>" class="img-fluid">
+      $image_count = count( $gallery_ids );
+
+      // Nếu không có ảnh
+      if ( empty( $gallery_ids ) ) :
+      ?>
+        <div class="col-12">
+          <img src="<?php echo wc_placeholder_img_src(); ?>" class="img-fluid">
+        </div>
+      <?php
+      endif;
+
+      foreach ( $gallery_ids as $image_id ) :
+        $img = wp_get_attachment_image_src( $image_id, 'large' );
+        if ( ! $img ) continue;
+        // Nếu chỉ có 1 ảnh → full cột
+        $col_class = ( $image_count === 1 ) ? 'col-12' : 'col-12 col-md-6';
+      ?>
+        <div class="<?php echo esc_attr( $col_class ); ?>">
+          <div class="zoom-box ratio ratio-1x1">
+            <img
+              src="<?php echo esc_url( $img[0] ); ?>"
+              alt="<?php the_title_attribute(); ?>"
+              loading="lazy"
+            >
           </div>
-        <?php
-        endif;
+        </div>
+      <?php endforeach; ?>
 
-        foreach ( $gallery_ids as $image_id ) :
-          $img = wp_get_attachment_image_src( $image_id, 'large' );
-          if ( ! $img ) continue;
-        ?>
-          <div class="col-12 col-md-6">
-            <div class="zoom-box ratio ratio-1x1">
-              <img
-                src="<?php echo esc_url( $img[0] ); ?>"
-                alt="<?php the_title_attribute(); ?>"
-                loading="lazy"
-              >
-            </div>
-          </div>
-        <?php endforeach; ?>
-
-      </div>
     </div>
+  </div>
+
 
     <!-- =====================
          INFO SECTION (4/12)
