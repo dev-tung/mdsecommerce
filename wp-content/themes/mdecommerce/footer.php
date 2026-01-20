5321ea6e8173f541a9617569395b4122607dc049
dev-tung <devtung27101992@gmail.com>
2026-01-20 09:41:55 +0700

    Update code

--- wp-content/themes/mdecommerce/footer.php
+++ wp-content/themes/mdecommerce/footer.php
@@ -58,26 +58,27 @@
 
                 <div class="row gy-4">
 
-                    <div class="col-12 col-md-4">
+                    <div class="col-12 col-md-3">
                         <h5 class="footer-brand">Manh Dung Sports</h5>
                         <a class="footer-desc d-block small text-decoration-none mb-2">Địa chỉ vòng xuyến Văn Giang Ecopark</a>
                         <a class="footer-desc d-block small text-decoration-none mb-2">Mã số DN 0901190162</a>
                         <a class="footer-desc d-block small text-decoration-none mb-2">Người đại diện Đỗ Sơn Tùng</a>
                         <img
                             src="<?php echo get_template_directory_uri(); ?>/assets/img/bct.png"
-                            class="bct-img mb-2 w-25"
+                            class="bct-img mb-2 w-50"
                         />
                     </div>
 
 
-                    <div class="col-12 col-md-3">
+                    <div class="col-12 col-md-2">
                         <h6 class="footer-title">Liên kết</h6>
                         <a href="/" class="footer-link d-block small mb-2">Trang chủ</a>
                         <a href="/shop" class="footer-link d-block small mb-2">Cửa hàng</a>
-                        <a href="/string" class="footer-link d-block small">Căng cước</a>
+                        <a href="/string" class="footer-link d-block small mb-2">Căng cước</a>
+                        <a href="/ctv" class="footer-link d-block small">Cộng tác viên</a>
                     </div>
 
-                    <div class="col-12 col-md-3">
+                    <div class="col-12 col-md-2">
                         <h6 class="footer-title">Bảo hành</h6>
                         <a class="footer-link d-block small mb-2" href="https://manhdungsports.store/chinh-sach-bao-hanh-vot-cau-long-yonex/" >Vợt Yonex</a>
                         <a class="footer-link d-block small mb-2" href="https://manhdungsports.store/chinh-sach-bao-hanh-vot-cau-long-li-ning/" >Vợt Lining</a>
@@ -101,6 +102,12 @@
                         <a href="https://www.youtube.com/@manhdungsports" target="_blank" class="footer-link d-block small">
                             YouTube
                         </a>
+                    </div>
+                    <!-- Google Map -->
+                    <div class="col-12 col-md-3">
+                        <h6 class="footer-title">Bản đồ cửa hàng</h6>
+
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3726.1301894345106!2d105.93952307625392!3d20.947286290570084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135afcc68063b9f%3A0x5b21b9e15c97cfa!2zQ-G6p3UgbMO0bmcgTeG6oW5oIETFqW5nIChNRCBCQURNSU5UT04gU0hPUCk!5e0!3m2!1sen!2s!4v1768876714999!5m2!1sen!2s" width="100%" height="220" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
 
                 </div>
